5d35d7581b9d2231b6539b5758b93aef60d9715e
ekgns14 <ekgns14@naver.com>
2021-02-05 20:15:02 +0900

    함수 이름 바꿈

--- changeButton/ViewController.swift
+++ changeButton/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var color:UIColor = UIColor.white
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func redChangeButton(_ sender: Any) {
+        self.view.backgroundColor = UIColor.red
+    }
+    
+    @IBAction func yellowChangeButton(_ sender: Any) {
+        self.view.backgroundColor = UIColor.yellow
+    }
+    
+    @IBAction func changeColorButton(_ sender: Any) {
+        if  self.view.backgroundColor == UIColor.red {
+            self.view.backgroundColor = UIColor.yellow
+        } else if self.view.backgroundColor == UIColor.yellow {
+            self.view.backgroundColor = UIColor.white
+        } else {
+            self.view.backgroundColor = UIColor.red
+        }
+    
+    }
 }
 
